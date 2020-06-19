b7d5a9abedff66d0e1165b5994692af570386cb0
kamathmanu <maniriddle@gmail.com>
2020-06-19 13:49:39 -0400

    Implement custom exception to wrap IOExceptions related to the scraper

---
+++ src/main/java/scraper/ScraperException.java
@@ -0,0 +1,14 @@
+package scraper;
+
+public class ScraperException extends Exception {
+    final String message;
+
+    public ScraperException (String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return this.message;
+    }
+}
