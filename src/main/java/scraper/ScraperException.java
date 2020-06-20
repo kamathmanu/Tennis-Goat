9b61766560a595fe4c5b9004cf1e16f9c52b7701
kamathmanu <manuriddle@gmail.com>
2020-06-20 14:25:04 -0400

    Extend ScraperException to chain subclass Exceptions resulting in cleaner exception handling

--- src/main/java/scraper/ScraperException.java
+++ src/main/java/scraper/ScraperException.java
@@ -5,6 +5,10 @@ public class ScraperException extends Exception {
     public ScraperException (String message) {
         this.message = message;
     }
+    public ScraperException (String message, Throwable cause) {
+        super(cause);
+        this.message = message;
+    }
     @Override
     public String toString() {
         return this.message;
