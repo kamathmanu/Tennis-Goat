c0e3f1910fe18c414d278c9b5717e7be742eaa73
kamathmanu <manuriddle@gmail.com>
2020-07-16 15:30:31 -0400

    Structure for tracking scores. TODO: provide access methods to modify the hashmap

--- src/main/java/rankallocator/PlayerScoring.java
+++ src/main/java/rankallocator/PlayerScoring.java
@@ -2,6 +2,19 @@ package rankallocator;
 
 // A wrapper class that uses a hashmap and additional metadata to maintain the
 // score of each No.1 player. The score is maintained using the PlayerValue abstraction
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayerScoring {
+    private Map<String, PlayerValue> playerScorer;
+
+    public PlayerScoring() {
+        playerScorer = new HashMap<>();
+    }
+
+    public Map<String, PlayerValue> getPlayerScorer() {
+        return playerScorer;
+    }
+
+    // functions to interface with the private hashmap
 }
